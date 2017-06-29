fe733e201e614894bb6836c60711d5192d8f37e3
FALLENau <reece.c.jones@gmail.com>
2017-06-29 09:57:00 +0100

    startpoint

---
+++ template_pattern/Bear.java
@@ -0,0 +1,21 @@
+public abstract class Bear {
+
+  public abstract void gatherFood();
+
+  public void wakeUp() {
+      System.out.println("Waking up");
+  }
+
+  public void eat() {
+    System.out.println("OM NOM NOM");
+  }
+
+  public void sleep() {
+    System.out.println("Zzz");
+  }
+
+  public void roar(){
+    System.out.println("roar!");
+  }
+
+}
