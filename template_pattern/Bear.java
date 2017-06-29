de860efcb2dbf742376fd8cfe0dbe37c3fe34661
FALLENau <reece.c.jones@gmail.com>
2017-06-29 10:04:15 +0100

    BearBEAR Bear

--- template_pattern/Bear.java
+++ template_pattern/Bear.java
@@ -1,5 +1,12 @@
 public abstract class Bear {
 
+  public void typicalDay() {
+      wakeUp();
+      gatherFood();
+      eat();
+      sleep();
+  }
+
   public abstract void gatherFood();
 
   public void wakeUp() {
--- template_pattern/Runner.java
+++ template_pattern/Runner.java
@@ -1,6 +1,11 @@
 public class Runner {
+
   public static void main(String[] args) {
     Bear bear = new PolarBear();
-    bear.roar();
+    bear.typicalDay();
+
+    Bear grizzlybear = new GrizzlyBear();
+    grizzlybear.typicalDay();
   }
+
 }
